bcc7cb5a4547469b4bc09f5babf82c5c594b4088
Steve Akinyemi <appcypher@outlook.com>
2017-11-20 07:15:56 +0100

    Revert "#152799104 Design Modal for Creating Events"

--- template/js/custom.js
+++
@@ -1,5 +0,0 @@
-// Opens the sidebar. The sidebar is collapsible only on small screens.
-function openSidebar() {}
-
-// Closes the sidebar. The sidebar is collapsible only on small screens.
-function closeSidebar() {}
