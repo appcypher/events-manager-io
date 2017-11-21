d17a3a3ccfb9aa56769254a72e85942f0d1fe71d
Steve Akinyemi <appcypher@outlook.com>
2017-11-21 10:35:33 +0100

    Fix typo

--- server/models/index.js
+++ server/models/index.js
@@ -8,8 +8,8 @@ const config = require(`${__dirname}/../config/config.json`)[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_constiable) {
-  sequelize = new Sequelize(process.env[config.use_env_constiable]);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
