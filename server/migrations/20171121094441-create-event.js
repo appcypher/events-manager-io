e837df7133928185fdaf5f894f9ce382059c75f4
Steve Akinyemi <appcypher@outlook.com>
2017-11-21 14:07:36 +0100

    Add sequelize migration files in es6

---
+++ server/migrations/20171121094441-create-event.js
@@ -0,0 +1,35 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('Events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      datetime: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      duration: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    }),
+  down: queryInterface => queryInterface.dropTable('Events'),
+};
