46e0b1c865a61896c326e7d3e1f640514154f879
Steve Akinyemi <appcypher@outlook.com>
2017-11-22 11:29:01 +0100

    Add token session verification

---
+++ server/middleware/authorization.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+
+/**
+ * Check if user is an admin  and has certain access privileges
+ * e.g modifying existing centers
+ * @param{Object} req - api request
+ * @param{Object} res - route response
+ * @param{Object} next - jumping to next handler
+ * @return{undefined}
+ */
+export default function checkUserAdmin(req, res, next) {
+  const token = req.body.token || req.headers.token;
+  if (!token) {
+    res.status(401).send({ status: 401, message: 'Session token is required!' });
+  } else {
+    // Check if token matches the one provided at login
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+      // Check id session is of admin
+      if (decoded.admin !== true) {
+        res.status(403).send({ status: 403, message: 'You don\'t have enough permission to access this route!' });
+      } else next();
+    });
+  }
+}
