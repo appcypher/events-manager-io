fc0083569be74c61112cab5c31cd56619c33964a
Steve Akinyemi <appcypher@outlook.com>
2017-11-22 07:23:09 +0100

    Add signup route

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,12 +3,19 @@ import path from 'upath';
 
 
 const router = express.Router();
-
+// Home
 router.route('/')
   .get((req, res) => {
     res.status(200).sendFile(path.join(__dirname, '/../../template/index.html'));
   });
 
+// Signup
+router.route('/api/v1/users/')
+  .post((req, res) => {
+    res.status(200).send({ id: 2 });
+  });
+
+// Login
 router.route('/api/v1/users/login')
   .post((req, res) => {
     res.status(200).send({ id: 2 });
