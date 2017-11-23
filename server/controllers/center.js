50481c7b5bdb54cb2fe302ad676f0351c81048d7
Steve Akinyemi <appcypher@outlook.com>
2017-11-23 17:41:28 +0100

    Add controller to modify event

--- server/controllers/center.js
+++ server/controllers/center.js
@@ -23,7 +23,33 @@ class EventCenterController {
           status: 201, message: 'Center created!', data: center,
         });
       })
-      .catch(err => res.status(400).send({ status: 400, message: err.errors[0].messsge || err }));
+      .catch(err => res.status(400).send({ message: err.errors[0].messsge || err }));
+  }
+
+  /**
+   * Modifies an existing center
+   * @param{Object} req - api request
+   * @param{Object} res - route response
+   * @return{json} new center details
+   */
+  static modifyCenter(req, res) {
+    EventCenter
+      .findOne({ where: { id: req.params.centerId } })
+      .then((center) => {
+        if (center) {
+          center.update({
+            name: req.body.name || center.name,
+            description: req.body.description || center.description,
+            type: req.body.type || center.type,
+            price: req.body.price || center.price,
+            location: req.body.location || center.location,
+          });
+          res.status(200).send({ message: 'Center modified!', data: center });
+        } else {
+          res.status(404).send({ message: 'Cannot find specified center!', data: center });
+        }
+      })
+      .catch(err => res.status(400).send({ message: err.errors[0].messsge || err }));
   }
 }
 
