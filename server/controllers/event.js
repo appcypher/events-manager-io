ceff5674e45b219695c5f57881726e7e1da1cceb
Steve Akinyemi <appcypher@outlook.com>
2017-11-24 08:30:41 +0100

    Add controller to modify selected event

--- server/controllers/event.js
+++ server/controllers/event.js
@@ -23,6 +23,31 @@ class EventController {
       })
       .catch(err => res.status(400).send({ message: err.errors[0].messsge || err }));
   }
+
+  /**
+   * Modifies an existing event
+   * @param{Object} req - api request
+   * @param{Object} res - route response
+   * @return{json}
+   */
+  static modifyEvent(req, res) {
+    Event
+      .findOne({ where: { id: req.params.eventId } })
+      .then((event) => {
+        if (event) {
+          event.update({
+            title: req.body.title || event.title,
+            description: req.body.description || event.description,
+            center: req.body.center || event.center,
+            date: req.body.date != null ? new Date(req.body.date).toISOString() : event.date,
+          });
+          res.status(200).send({ message: 'event modified!', data: event });
+        } else {
+          res.status(404).send({ message: 'cannot find specified event!' });
+        }
+      })
+      .catch(err => res.status(400).send({ message: err.errors[0].messsge || err }));
+  }
 }
 
 export default EventController;
