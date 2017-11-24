d3367eeb87be753b990127ac9e75a93c5af2f66e
Steve Akinyemi <appcypher@outlook.com>
2017-11-24 13:49:23 +0100

    Add signup tests

--- tests/api/users.js
+++ tests/api/users.js
@@ -1,21 +1,16 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../../server/server';
+/* eslint-disable no-unused-expressions  */
 
 chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Users', () => {
-  const userSignup = {
-    username: 'jegede', password: 'jinadu', email: 'jinadu@yahoo.com', fullname: 'Jegede Jinadu',
-  };
-
-  const userLogin = {
-    username: 'jegede', password: 'jinadu',
-  };
-
-  // First sign up should be successful
-  it('(POST /api/v1/users) should create user', (done) => {
+  it('(POST /api/v1/users) should return 201 if successful', (done) => {
+    const userSignup = {
+      username: 'jegede', password: 'jegede', email: 'jegede@yahoo.com', fullname: 'Jegede Jinadu',
+    };
     chai.request(server)
       .post('/api/v1/users')
       .send(userSignup)
@@ -27,13 +22,31 @@ describe('Users', () => {
       });
   });
 
-  // Signing up with the same email and password
-  it('(POST /api/v1/users) should not create user on when email and username is reused', (done) => {
+  it('(POST /api/v1/users) should return 409 if username already exists', (done) => {
+    const userSignupExisting = {
+      username: 'jegede', password: 'jinadu', email: 'jinadu@yahoo.com', fullname: 'Jegede Jinadu',
+    };
     chai.request(server)
       .post('/api/v1/users')
-      .send(userSignup)
+      .send(userSignupExisting)
       .end((err, res) => {
         expect(res).to.have.status(409);
+        expect(res.body.message).to.equal('username already taken!');
+        expect(res).to.be.json;
+        done();
+      });
+  });
+
+  it('(POST /api/v1/users) should 409 if username already exists', (done) => {
+    const userSignupExisting = {
+      username: 'jinadu', password: 'jinadu', email: 'jegede@yahoo.com', fullname: 'Jegede Jinadu',
+    };
+    chai.request(server)
+      .post('/api/v1/users')
+      .send(userSignupExisting)
+      .end((err, res) => {
+        expect(res).to.have.status(409);
+        expect(res.body.message).to.equal('email already taken!');
         expect(res).to.be.json;
         done();
       });
