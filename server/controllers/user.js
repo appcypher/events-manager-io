12a40a8424a9f767377ab93424b45ce5185c13ad
Steve Akinyemi <appcypher@outlook.com>
2017-11-26 05:27:36 +0100

    Remove createAdminUser controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -28,7 +28,7 @@ class UserController {
           process.env.SECRET_KEY,
           { expiresIn: '30m' },
         );
-        res.status(201).send({ message: 'user created!', data: user, token });
+        res.status(201).send({ message: 'user created!', token });
       })
       .catch(err => res.status(400).send({ message: err.errors[0].message || err }));
   }
@@ -53,36 +53,6 @@ class UserController {
         res.status(200).send({ message: 'user logged in!', token });
       })
       .catch(err => res.status(400).send({ message: err.errors[0].message || err }));
-  }
-
-  /**
-   * Creates an admin user with a hashed password
-   * @param{Object} req - api request
-   * @param{Object} res - route response
-   * @return{string} registered user details
-   */
-  static createAdminUser(req, res) {
-    // Hash password to save in the database
-    const passwordHash = bcrypt.hashSync('admin', 10);
-    User
-      .create({
-        username: 'admin',
-        password: passwordHash,
-        fullname: 'Steve Akinyemi',
-        email: 'admin@events-manager-io.herokuapp.com',
-        admin: true,
-      })
-      .then((user) => {
-        const token = jwt.sign( // Create a session token with 30-minute session
-          { id: user.id, admin: true },
-          process.env.SECRET_KEY,
-          { expiresIn: '30m' },
-        );
-        res.status(201).send({
-          status: 201, message: 'Admin user created!', data: user, token,
-        });
-      })
-      .catch(err => res.status(400).send({ status: 400, message: err.errors[0].messsge || err }));
   }
 }
 
