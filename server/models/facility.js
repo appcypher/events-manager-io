ba440a68f494e6dec215c68853cabce6b7b554a8
Steve Akinyemi <appcypher@outlook.com>
2017-11-27 13:25:31 +0100

    Add db validations

--- server/models/facility.js
+++ server/models/facility.js
@@ -8,30 +8,57 @@ export default (sequelize, DataTypes) => {
     },
     projector: {
       type: DataTypes.BOOLEAN,
+      validate: {
+        isBoolean: { args: true, msg: 'projector format is invalid! - make sure value is either true or false!' },
+      },
     },
     chairs: {
       type: DataTypes.INTEGER,
+      validate: {
+        isInt: { args: true, msg: 'chairs format is invalid! - make sure value is an integer' },
+      },
     },
     tables: {
       type: DataTypes.INTEGER,
+      validate: {
+        isInt: { args: true, msg: 'tables format is invalid! - make sure value is an integer' },
+      },
     },
     parkinglot: {
       type: DataTypes.INTEGER,
+      validate: {
+        isInt: { args: true, msg: 'parkinglot format is invalid! - make sure value is an integer' },
+      },
     },
     restrooms: {
       type: DataTypes.BOOLEAN,
+      validate: {
+        isBoolean: { args: true, msg: 'restrooms format is invalid! - make sure value is either true or false!' },
+      },
     },
     telescreens: {
       type: DataTypes.BOOLEAN,
+      validate: {
+        isBoolean: { args: true, msg: 'telescreens format is invalid! - make sure value is either true or false!' },
+      },
     },
     lighting: {
       type: DataTypes.BOOLEAN,
+      validate: {
+        isBoolean: { args: true, msg: 'lighting format is invalid! - make sure value is either true or false!' },
+      },
     },
     sounds: {
       type: DataTypes.BOOLEAN,
+      validate: {
+        isBoolean: { args: true, msg: 'sounds format is invalid! - make sure value is either true or false!' },
+      },
     },
     stage: {
       type: DataTypes.BOOLEAN,
+      validate: {
+        isBoolean: { args: true, msg: 'stage format is invalid! - make sure value is either true or false!' },
+      },
     },
     centerId: {
       type: DataTypes.INTEGER,
