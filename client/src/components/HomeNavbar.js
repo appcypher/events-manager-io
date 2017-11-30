a943ebcc6355dc6de0d6340451385220918720f7
Steve Akinyemi <appcypher@outlook.com>
2017-11-30 12:24:34 +0100

    Add Navbar  components

---
+++ client/src/components/HomeNavbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from './Navbar';
+
+import brand from '../assets/icons/brand.png';
+
+/**
+ * Has the home navbar styles and children
+ * @param{undefined}
+ * @return{React.Component}
+ */
+const HomeNavbar = () => (
+  <Navbar className="io-navbar io-fixed-top">
+    <Link href to="/" className="io-brand"> 
+      <div className="io-text">EventsManagerIO</div>
+      <img src={brand} alt="" className="io-img" />
+    </Link>
+    <div className="io-middle io-start">
+      <a href="#contact">CONTACT</a>
+      <a href="#about">ABOUT US</a>
+    </div>
+    <div className="io-end">
+      <Link href to="/signin" className="io-text">LOGIN</Link>
+      <Link href to="/signup" className="io-text">SIGNUP</Link>
+    </div>
+  </Navbar>
+);
+
+export default HomeNavbar;
