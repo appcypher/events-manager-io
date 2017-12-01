b0fe55818f07fea10e3745ac0eed968175d904e8
Steve Akinyemi <appcypher@outlook.com>
2017-12-01 22:13:08 +0100

    Add information to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 /**
  * Component for ceating navbars
@@ -6,28 +7,24 @@ import React from 'react';
  * @return{React.Component}
  */
 const Footer = () => (
-  <div className="io-footer">
+  <div id="footer" className="io-footer">
     <div className="container-fluid">
       <div className="row io-top-row justify-content-center">
-        <div className="col-4">
-          <p>Lorem ipsum dolor sit amet</p>
-          <p>Lorem ipsum dolor sit amet</p>
-          <p>Lorem ipsum dolor sit amet</p>
-          <p>Lorem ipsum dolor sit amet</p>
+        <div className="col-6">
+          <h3> CONTACT </h3>
+          <p>appcypher@outlook.com</p>
+          <p>+234 234 567 8901</p>
+          <p>+234 099 765 4321</p>
+          <p>Lagos, Nigeria.</p>
         </div>
-        <div className="col-4">
-          <p>Lorem ipsum dolor sit amet</p>
-          <p>Lorem ipsum dolor sit amet</p>
-          <p>Lorem ipsum dolor sit amet</p>
-          <p>Lorem ipsum dolor sit amet</p>
-          <p>Lorem ipsum dolor sit amet</p>
-        </div>
-        <div className="col-4">
-          <p>Lorem ipsum dolor sit amet</p>
-          <p>Lorem ipsum dolor sit amet</p>
-          <p>Lorem ipsum dolor sit amet</p>
-          <p>Lorem ipsum dolor sit amet</p>
-          <p>Lorem ipsum dolor sit amet</p>
+        <div className="col-6">
+          <h3> NAVIGATE </h3>
+          <Link href to="/signin">Login</Link>
+          <Link href to="/signout">Signup</Link>
+          <Link href to="/discover">Discover</Link>
+          <Link href to="/profile">Profile</Link>
+          <Link href to="/home#about">About</Link>
+          <Link href to="/home#footer">Contact</Link>
         </div>
       </div>
     </div>
