e14ee9d1dbd1466f0a177c49bd8fa1a46bbabd86
Steve Akinyemi <appcypher@outlook.com>
2017-12-02 23:09:59 +0100

    Reduce sloc in AddCenterModal

--- client/src/components/AddCenterModal.js
+++ client/src/components/AddCenterModal.js
@@ -72,8 +72,7 @@ class AddCenterModal extends React.Component {
               <ModalSection title="Facilities" extra="io-start"><ModalList list={facilityList} saveInput={this.saveInput} /></ModalSection>
             </form>
           </div>
-          <div className="io-footer">
-            <button id="add-center-cancel" className="io-submit-btn io-sm" onClick={this.cancel}>CANCEL</button>
+          <div className="io-footer"><button id="add-center-cancel" className="io-submit-btn io-sm" onClick={this.cancel}>CANCEL</button>
             <button id="add-center-submit" className="io-submit-btn io-sm" onClick={this.submit}>SUBMIT</button>
           </div>
         </div>
