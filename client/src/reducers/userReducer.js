d7bfd178f9c4164762cea59282a5210a4fe67de8
Steve Akinyemi <appcypher@outlook.com>
2017-12-02 01:30:57 +0100

    Add support for signup actions

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,20 +1,37 @@
 const initial = {
-  data: [],
-  err: {
-    response: null,
-  },
+  message: '', token: '',
 };
 
+/**
+ * Returns a state containing a message and (possibly) a user property
+ * @param{Object} state - previous state
+ * @param{Object} action - action containing a type and payload property
+ * @return{Object} new state
+ */
 export default (state = initial, { type, payload }) => {
   switch (type) {
-    case 'USER_LOGIN_SUCCESSFUL': {
+    case 'USER_SIGNUP_SUCCESSFUL': {
+      const { message, token } = payload;
       return {
-        ...state, data: payload,
+        ...state, message, token,
+      };
+    }
+    case 'USER_SIGNUP_FAILED': {
+      const { message } = payload;
+      return {
+        ...state, message,
+      };
+    }
+    case 'USER_LOGIN_SUCCESSFUL': {
+      const { message, token } = payload;
+      return {
+        ...state, message, token,
       };
     }
     case 'USER_LOGIN_FAILED': {
+      const { message } = payload;
       return {
-        ...state, err: payload,
+        ...state, message,
       };
     }
     default: return state;
