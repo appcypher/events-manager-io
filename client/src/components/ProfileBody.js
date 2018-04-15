5a7a40e53eba8b0ca2adcee70fceb7fa65c33daa
Steve Akinyemi <appcypher@outlook.com>
2018-04-15 22:23:45 +0100

    Add implementation code

--- client/src/components/ProfileBody.js
+++ client/src/components/ProfileBody.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import ProfileUserCard from './ProfileUserCard';
+import ProfileEventCardRow from './ProfileEventCardRow';
 
-const ProfileBody = () => <div />;
+class ProfileBody extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="io-body-content">
+        <div className="container-fluid">
+          <ProfileUserCard />
+          <ProfileEventCardRow />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ProfileBody;
