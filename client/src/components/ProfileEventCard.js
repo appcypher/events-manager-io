b64a60f78835424a12f4880fdae680fe77e4cb39
Steve Akinyemi <appcypher@outlook.com>
2018-04-15 22:19:24 +0100

    Add support for placeholder image

--- client/src/components/ProfileEventCard.js
+++ client/src/components/ProfileEventCard.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import placeholder from '../assets/images/placeholder.jpg';
 
 const ProfileEventCard = (props) => {
   const {
-    imageurl, title, centername, location, date, time,
+    title, centerName, location, date, time,
   } = props;
 
+  let { imageUrl } = props;
+
+  // If image is not provided, use a placeholder image
+  if (!imageUrl) {
+    imageUrl = placeholder;
+  }
+
   return (
-    <div className="io-event-card"><img alt="" src={imageurl} />
+    <div className="io-event-card"><img alt="" src={imageUrl} />
       <div className="io-content">
         <div className="io-details">
           <div className="io-header">
@@ -14,7 +22,7 @@ const ProfileEventCard = (props) => {
             <i className="io-icon fa fa-pencil-square-o" />
             <i className="io-icon fa trash" />
           </div>
-          <p>{centername}</p>
+          <p>{centerName}</p>
           <p className="io-location">{location}</p>
         </div>
         <div className="io-date-time"><span className="io-date">{date}</span><span className="io-time">{time}</span></div>
