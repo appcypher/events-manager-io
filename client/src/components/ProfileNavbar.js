9fc3bb38978128591c62ab95068246940380aeb7
Steve Akinyemi <appcypher@outlook.com>
2018-04-15 22:24:51 +0100

    Refactor code

--- client/src/components/ProfileNavbar.js
+++ client/src/components/ProfileNavbar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Navbar from './Navbar';
+import UserAction from '../actions/userActions';
+import history from '../index';
 
 import brand from '../assets/icons/brand.png';
 
@@ -9,27 +12,55 @@ import brand from '../assets/icons/brand.png';
  * @param{undefined}
  * @return{React.Component}
  */
-const ProfileNavbar = () => (
-  <Navbar classNameName="io-navbar io-fixed-top">
-    <Link href to="/" className="io-brand">
-      <div className="io-text">EventsManagerIO</div>
-      <img alt="" src={brand} className="io-img" />
-    </Link>
-    <div className="io-middle">
-      <input placeholder="Type keyword here" type="text" className="io-search-bar" />
-      <i className="io-search-button fa fa-search" />
-    </div>
-    <div className="io-end">
-      <Link href to="/discover">
-        <i className="io-icon fa fa-list" />
-        <div className="io-text io-switchable">DISCOVER</div>
-      </Link>
-      <i className="io-icon fa fa-bell" />
-      <div className="io-text io-switchable">MESSAGES</div>
-      <i className="io-icon fa fa-cog" />
-      <div className="io-text io-switchable">SETTINGS</div>
-    </div>
-  </Navbar>
-);
+class ProfileNavbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-export default ProfileNavbar;
+  logoutUser = () => {
+    // Remove user details in localStorage.
+    localStorage.setItem('user.token', 'undefined');
+    localStorage.setItem('user.admin', 'undefined');
+
+    // Remove all session data.
+    this.props.logoutUser();
+
+    // Change page.
+    history.push('/');
+  }
+
+  render() {
+    return (
+      <Navbar classNameName="io-navbar io-fixed-top">
+        <Link href to="/" className="io-brand">
+          <div className="io-text">EventsManagerIO</div>
+          <img alt="" src={brand} className="io-img" />
+        </Link>
+        <div className="io-middle">
+          <input placeholder="Type keyword here" type="text" className="io-search-bar" />
+          <i className="io-search-button fa fa-search" />
+        </div>
+        <div className="io-end">
+          <Link href to="/discover">
+            <i className="io-icon fa fa-list" />
+            <div className="io-text io-switchable">DISCOVER</div>
+          </Link>
+          <i className="io-icon fa fa-bell" />
+          <div className="io-text io-switchable">MESSAGES</div>
+          <i className="io-icon fa fa-cog" />
+          <div className="io-text io-switchable" onClick={this.logoutUser}>LOGOUT</div>
+        </div>
+      </Navbar>
+    );
+  }
+}
+
+const mapStateToProps = ({ user }) => ({ user });
+
+export default connect(
+  mapStateToProps,
+  {
+    logoutUser: UserAction.logoutUser,
+  },
+)(ProfileNavbar);
--- client/src/components/ProfileUserCard.js
+++ client/src/components/ProfileUserCard.js
@@ -1,24 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import placeholder from '../assets/images/placeholder.jpg';
 
-const ProfileUserCard = (props) => {
-  const {
-    imageurl, tagline, fullname, description,
-  } = props;
+class ProfileUserCard extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="io-col-center">
-      <div className="io-profile-card">
-        <div className="io-header">
-          <div className="io-name">{fullname} </div>
-          <div className="io-spacer" />
-          <i className="io-icon fa fa-pencil-square-o" />
+    let imageUrl = this.props.user.user.picture;
+
+    // If image is not provided, use a placeholder image
+    if (!imageUrl) {
+      imageUrl = placeholder;
+    }
+
+    this.state = { ...this.props.user.user, imageUrl };
+  }
+
+  render() {
+    const {
+      imageUrl, tagline, fullname, description,
+    } = this.state;
+
+    return (
+      <div className="io-col-center">
+        <div className="io-profile-card">
+          <div className="io-header">
+            <div className="io-name">{fullname} </div>
+            <div className="io-spacer" />
+            <i className="io-icon fa fa-pencil-square-o" />
+          </div>
+          <div className="io-short">{tagline}</div>
+          <div className="io-long">{description}</div>
+          <img alt="profile_img" src={imageUrl} className="io-img" />
         </div>
-        <div className="io-short">{tagline}</div>
-        <div className="io-long">{description}</div>
-        <img alt="profile_img" src={imageurl} className="io-img" />
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default ProfileUserCard;
+const mapStateToProps = ({ user }) => ({ user });
+
+
+export default connect(
+  mapStateToProps,
+  {},
+)(ProfileUserCard);
