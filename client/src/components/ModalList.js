5fa9daec97bafc5e4d9e50b6e36421e81e016d3f
Steve Akinyemi <appcypher@outlook.com>
2018-04-15 22:23:10 +0100

    Fix react li identifying issue

--- client/src/components/ModalList.js
+++ client/src/components/ModalList.js
@@ -3,9 +3,11 @@ import React from 'react';
 const ModalList = (props) => {
   const listElements = [];
 
+  let index = 0;
+
   props.list.forEach((item) => {
     const name = item.toLowerCase().replace(/\s+/g, '');
-    listElements.push(<li><input type="checkbox" name={name} onChange={props.saveInput} /><span className="io-check-text">{item}</span></li>);
+    listElements.push(<li key={index += 1}><input type="checkbox" name={name} onChange={props.saveInput} /><span className="io-check-text">{item}</span></li>);
   });
 
   return (
